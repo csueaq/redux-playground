99287d579ec44902f0eb714d05b5c26fd58f6091
Izzy <xinjin214@gmail.com>
2015-10-13 22:17:39 +0100

    refactoring

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,7 +1,7 @@
 /**
  * Created by Izzy on 28/02/15.
  */
-'use strict'
+'use strict';
 
 var Router = require('koa-router');
 var React = require('react');
@@ -11,26 +11,7 @@ import { createStore } from 'redux'
 import  counter  from '../app/js/reducer/counterReducer'
 import App from '../app/js/app.jsx'
 import { Provider } from 'react-redux'
-
-
-
-function renderFullPage(html, initialState) {
-    return `
-    <!doctype html>
-    <html>
-      <head>
-        <title>Redux Universal Example</title>
-      </head>
-      <body>
-        <div id="content">${html}</div>
-        <script>
-          window.__INITIAL_STATE__ = ${JSON.stringify(initialState)};
-        </script>
-        <script src="/js/bundle.js"></script>
-      </body>
-    </html>
-    `;
-}
+import renderFullPage from '../serverRendering/renderIndex'
 
 
 const store = createStore(counter);
