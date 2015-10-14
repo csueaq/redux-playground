051ee8d1a70e0dcfabf1133331a6b44ee4d41654
Xin Jin <xin.jin@net-a-porter.com>
2015-10-14 18:38:07 +0100

    some comments

--- src/routers/index.js
+++ src/routers/index.js
@@ -6,6 +6,7 @@
 var Router = require('koa-router');
 var React = require('react');
 var indexRouter = new Router();
+import { Map } from 'immutable'
 
 import { createStore } from 'redux'
 import  counter  from '../app/js/reducer/counterReducer'
@@ -14,14 +15,18 @@ import { Provider } from 'react-redux'
 import renderFullPage from '../serverRendering/renderFullPage'
 
 
+/*  This is how to set initial state from the server and the client will read it after first render
+    const initialState = Map({count:10});
+    const store = createStore(counter,initialState);
+*/
 const store = createStore(counter);
+const initialState = store.getState();
 
 // Render the component to a string
 const html = React.renderToString(
         <App store={store}/>
 );
 
-const initialState = store.getState();
 
 
 indexRouter.get('/', function *() {
