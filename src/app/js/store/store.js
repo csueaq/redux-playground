ef614909db13f12424c41458eef946c8c1426605
Xin Jin <xin.jin@net-a-porter.com>
2015-10-15 11:15:16 +0100

    check window.initialState before using it

--- src/app/js/store/store.js
+++ src/app/js/store/store.js
@@ -7,7 +7,11 @@ import  counter  from '../reducer/counterReducer'
 import { Map } from 'immutable'
 
 export default function () {
-    const initialState = Map(window.__INITIAL_STATE__);
-    return createStore(counter,initialState);
+    if(!window.__INITIAL_STATE__)
+        return createStore(counter);
+    else {
+        const initialState = Map(window.__INITIAL_STATE__);
+        return createStore(counter,initialState);
+    }
 }
 
